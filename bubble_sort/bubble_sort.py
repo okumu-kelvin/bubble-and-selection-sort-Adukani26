7790a0731f034eb0a99405adb1ba5ec41d45dfb7
Adukani26 <adan.konse@strathmore.edu>
2025-06-20 10:34:52 +0300

    Update bubble_sort.py
    added bubble-sort-code

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,14 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+    def bubble_sort(unsorted_list):
+    # TODO: Implement bubble sort
+    n = len(unsorted_list)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                swapped = True
+        if not swapped:
+            break
+    return unsorted_list
