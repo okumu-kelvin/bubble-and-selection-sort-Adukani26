6aa575b4431513963f9988178294afd84c00fea3
Adukani26 <adan.konse@strathmore.edu>
2025-06-20 10:48:13 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,7 +1,5 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
     n = len(unsorted_list)
     for i in range(n):
         swapped = False
